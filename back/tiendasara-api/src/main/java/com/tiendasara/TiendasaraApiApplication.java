294a4a2eeb3a367e026bdc7f3fad3b86d7d7a8f2
Angel Antonio Guzman Leyva <4n70n1017xd@gmail.com>
2026-06-07 19:40:33 -0600

    Se agrega springboot al proyecto

---
+++ back/tiendasara-api/src/main/java/com/tiendasara/TiendasaraApiApplication.java
@@ -0,0 +1,13 @@
+package com.tiendasara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendasaraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendasaraApiApplication.class, args);
+	}
+
+}
---
+++ back/tiendasara-api/src/test/java/com/tiendasara/TiendasaraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendasara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendasaraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
